9633c958ac7fe5e557ad3be1631764cbf0b56339
sudokoko <joebiden@kitgaming67.com>
2023-05-04 22:11:06 -0400

    Add permission level display, do not display RPC if banned

---
+++ PLRPC/Extensions/PermissionLevelExtensions.cs
@@ -0,0 +1,20 @@
+﻿using LBPUnion.PLRPC.Types;
+
+namespace LBPUnion.PLRPC.Extensions;
+
+public static class PermissionLevelExtensions
+{
+    public static string ToPrettyString(this PermissionLevel? level)
+    {
+        return level switch
+        {
+            PermissionLevel.Banned => " (Banned)",
+            PermissionLevel.Restricted => " (Restricted)",
+            PermissionLevel.Silenced => " (Silenced)",
+            PermissionLevel.Default => "", // default PermissionLevel doesn't really have a pretty name
+            PermissionLevel.Moderator => " (Moderator)",
+            PermissionLevel.Administrator => " (Administrator)",
+            _ => " (Unknown)",
+        };
+    }
+}
