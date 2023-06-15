30759ccc611dbd9ba4df8dfd76d8b5a730584d1f
koko <koko@drones.gay>
2023-06-15 17:38:40 -0400

    Implement custom application ID definition (#18)
    * Implement custom app ID and clean up config/args code
    
    * Remove unneeded comment
    
    * Remove errant whitespace
    
    * Fix null command arguments check

--- PLRPC/Types/PlrpcConfiguration.cs
+++ PLRPC/Types/PlrpcConfiguration.cs
@@ -5,8 +5,11 @@ namespace LBPUnion.PLRPC.Types;
 public class PlrpcConfiguration
 {
     [JsonPropertyName("serverUrl")]
-    public string? ServerUrl { get; set; }
+    public string ServerUrl { get; set; } = "https://lighthouse.lbpunion.com";
 
     [JsonPropertyName("username")]
-    public string? Username { get; set; }
+    public string Username { get; set; } = "";
+
+    [JsonPropertyName("applicationId")]
+    public string ApplicationId { get; set; } = "1060973475151495288";
 }
