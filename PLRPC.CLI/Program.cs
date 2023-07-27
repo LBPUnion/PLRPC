11c2c59dc41c1770e2eace8d69f7764b0390501e
sudokoko <koko@drones.gay>
2023-07-27 13:19:31 -0400

    Detach CLI from main project and convert main project into a class library

---
+++ PLRPC.CLI/Program.cs
@@ -0,0 +1,12 @@
+﻿using CommandLine;
+
+namespace LBPUnion.PLRPC.CLI;
+
+public static class Program
+{
+    public static async Task Main(string[] args)
+    {
+        ParserResult<Initializers.CommandLineArguments> result = Parser.Default.ParseArguments<Initializers.CommandLineArguments>(args);
+        await result.MapResult(async arguments => await new Initializers().ProcessArguments(arguments), _ => Task.CompletedTask);
+    }
+}
