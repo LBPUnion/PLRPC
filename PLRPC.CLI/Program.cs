23d2949a3b90980af69ab3660b22a1f990e5d689
sudokoko <koko@drones.gay>
2023-07-27 13:22:08 -0400

    CLI Program styling and structure nitpicks

--- PLRPC.CLI/Program.cs
+++ PLRPC.CLI/Program.cs
@@ -4,9 +4,15 @@ namespace LBPUnion.PLRPC.CLI;
 
 public static class Program
 {
+    private static ParserResult<Initializers.CommandLineArguments> result;
+
     public static async Task Main(string[] args)
     {
-        ParserResult<Initializers.CommandLineArguments> result = Parser.Default.ParseArguments<Initializers.CommandLineArguments>(args);
-        await result.MapResult(async arguments => await new Initializers().ProcessArguments(arguments), _ => Task.CompletedTask);
+        result = Parser.Default.ParseArguments<Initializers.CommandLineArguments>(args);
+        
+        await result.MapResult(async arguments =>
+        {
+            await new Initializers().ProcessArguments(arguments);
+        }, _ => Task.CompletedTask);
     }
 }
