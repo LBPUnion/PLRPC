5695c694fd10cabf4e7d00c5912ed3bcda216d35
koko <koko@drones.gay>
2023-08-09 17:24:24 -0400

    Move log builder to a subclass and add log method documentation

--- PLRPC/Logger.cs
+++ PLRPC/Logger.cs
@@ -18,77 +18,121 @@ public class Logger
     
     private readonly ILogger logger = loggerConfiguration.CreateLogger();
 
-    /*
-     * Used for generating a universal logging template
-     */
+    /// <summary>
+    /// Write debug information to the sink.
+    /// </summary>
+    /// <param name="message">String to be logged.</param>
+    /// <param name="logArea">Log area to be referenced.</param>
+    public void Debug(string message, LogArea logArea)
+    {
+        this.logger.Debug(LoggerUtils.BuildMessage(message, logArea));
+    }
 
-    private static string BuildLogMessage(string message, LogArea logArea)
+    /// <summary>
+    /// Write verbose information to the sink.
+    /// </summary>
+    /// <param name="message">String to be logged.</param>
+    /// <param name="logArea">Log area to be referenced.</param>
+    public void Verbose(string message, LogArea logArea)
+    {
+        this.logger.Verbose(LoggerUtils.BuildMessage(message, logArea));
+    }
+
+    /// <summary>
+    /// Write information to the sink.
+    /// </summary>
+    /// <param name="message">String to be logged.</param>
+    /// <param name="logArea">Log area to be referenced.</param>
+    public void Information(string message, LogArea logArea)
+    {
+        this.logger.Information(LoggerUtils.BuildMessage(message, logArea));
+    }
+
+    /// <summary>
+    /// Write a warning to the sink.
+    /// </summary>
+    /// <param name="message">String to be logged.</param>
+    /// <param name="logArea">Log area to be referenced.</param>
+    public void Warning(string message, LogArea logArea)
+    {
+        this.logger.Warning(LoggerUtils.BuildMessage(message, logArea));
+    }
+
+    /// <summary>
+    /// Write an error to the sink.
+    /// </summary>
+    /// <param name="message">String to be logged.</param>
+    /// <param name="logArea">Log area to be referenced.</param>
+    public void Error(string message, LogArea logArea)
+    {
+        this.logger.Error(LoggerUtils.BuildMessage(message, logArea));
+    }
+
+    /// <summary>
+    /// Write an error with an exception to the sink.
+    /// </summary>
+    /// <param name="message">String to be logged.</param>
+    /// <param name="logArea">Log area to be referenced.</param>
+    /// <param name="exception">Exception object to be logged.</param>
+    public void Error(string message, LogArea logArea, Exception exception)
+    {
+        this.logger.Error(exception, LoggerUtils.BuildMessage(message, logArea));
+    }
+
+    /// <summary>
+    /// Write a fatal error to the sink.
+    /// </summary>
+    /// <param name="message">String to be logged.</param>
+    /// <param name="logArea">Log area to be referenced.</param>
+    public void Fatal(string message, LogArea logArea)
+    {
+        this.logger.Fatal(LoggerUtils.BuildMessage(message, logArea));
+    }
+
+    /// <summary>
+    /// Write a fatal error with an exception to the sink.
+    /// </summary>
+    /// <param name="message">String to be logged.</param>
+    /// <param name="logArea">Log area to be referenced.</param>
+    /// <param name="exception">Exception object to be logged.</param>
+    public void Fatal(string message, LogArea logArea, Exception exception)
+    {
+        this.logger.Fatal(exception, LoggerUtils.BuildMessage(message, logArea));
+    }
+
+    /// <summary>
+    /// Write a log event to the sink manually.    
+    /// </summary>
+    /// <param name="logEvent">The log event to write.</param>
+    public void Write(LogEvent logEvent)
+    {
+        this.logger.Write(logEvent);
+    }
+}
+
+public abstract class LoggerUtils : Logger
+{
+    /// <summary>
+    /// Specifies the max length to pad the log area string in the message builder.
+    /// </summary>
+    private static readonly int maxLogAreaLength = Enum.GetNames<LogArea>().Max(x => x.Length);
+
+    /// <summary>
+    /// Build a log message with the specified log area.
+    /// </summary>
+    /// <param name="message">String to be logged.</param>
+    /// <param name="logArea">Log area to be referenced.</param>
+    /// <returns></returns>
+    public static string BuildMessage(string message, LogArea logArea)
     {
         StringBuilder sb = new();
 
-        string formattedLogArea = logArea.ToString().PadRight(20).Pastel(Color.LightBlue);
+        string formattedLogArea = logArea.ToString().PadRight(maxLogAreaLength).Pastel(Color.LightBlue);
         string formattedMessage = message.Pastel(Color.White);
 
         sb.Append($"[{formattedLogArea}] ".Pastel(Color.DimGray));
         sb.Append(formattedMessage);
 
         return sb.ToString();
-    }
-
-    /*
-     * Log levels that cannot have embedded exceptions
-     */
-
-    public void Debug(string message, LogArea logArea)
-    {
-        this.logger.Debug(BuildLogMessage(message, logArea));
-    }
-
-    public void Verbose(string message, LogArea logArea)
-    {
-        this.logger.Verbose(BuildLogMessage(message, logArea));
-    }
-
-    public void Information(string message, LogArea logArea)
-    {
-        this.logger.Information(BuildLogMessage(message, logArea));
-    }
-
-    public void Warning(string message, LogArea logArea)
-    {
-        this.logger.Warning(BuildLogMessage(message, logArea));
-    }
-
-    /*
-     * Log levels that can have embedded exceptions
-     */
-
-    public void Error(string message, LogArea logArea)
-    {
-        this.logger.Error(BuildLogMessage(message, logArea));
-    }
-
-    public void Error(string message, LogArea logArea, Exception exception)
-    {
-        this.logger.Error(exception, BuildLogMessage(message, logArea));
-    }
-
-    public void Fatal(string message, LogArea logArea)
-    {
-        this.logger.Fatal(BuildLogMessage(message, logArea));
-    }
-
-    public void Fatal(string message, LogArea logArea, Exception exception)
-    {
-        this.logger.Fatal(exception, BuildLogMessage(message, logArea));
-    }
-
-    /*
-     * Manually write a log event
-     */
-
-    public void Write(LogEvent logEvent)
-    {
-        this.logger.Write(logEvent);
     }
 }
