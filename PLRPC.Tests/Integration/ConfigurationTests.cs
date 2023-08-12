f5fd516096757f87eb0a8d0ce1f764a980b98555
koko <koko@drones.gay>
2023-08-12 07:46:44 -0400

    Implement Refresh integration testing

--- PLRPC.Tests/Integration/ConfigurationTests.cs
+++ PLRPC.Tests/Integration/ConfigurationTests.cs
@@ -1,4 +1,5 @@
 ﻿using LBPUnion.PLRPC.Types.Configuration;
+using LBPUnion.PLRPC.Types.Enums;
 using Xunit;
 
 namespace LBPUnion.PLRPC.Tests.Integration;
@@ -8,7 +9,6 @@ public class ConfigurationTests
 {
     private static readonly HttpClient unitTestClient = new()
     {
-        BaseAddress = new Uri("https://lighthouse.lbpunion.com/api/v1/"),
         DefaultRequestHeaders =
         {
             {
@@ -16,12 +16,14 @@ public class ConfigurationTests
             },
         },
     };
-
+    
     private static readonly Configuration lighthouseConfig = new(unitTestClient, new Logger());
     
     [Fact(Skip = "Remote configuration is not yet implemented into Lighthouse")]
-    public async void CanRetrieveAndParseRemoteConfiguration()
+    public async void CanRetrieveAndParseLighthouseConfiguration()
     {
+        unitTestClient.BaseAddress = new Uri("https://lighthouse.lbpunion.com/api/v1/");
+        
         RemoteConfiguration? remoteConfiguration = await lighthouseConfig.GetRemoteConfiguration();
         
         if (remoteConfiguration == null)
@@ -31,5 +33,22 @@ public class ConfigurationTests
 
         // TODO: Add more conditions here when API is implemented to Beacon
         Assert.Equal("1060973475151495288", remoteConfiguration.ApplicationId);
+    }
+
+    [Fact]
+    public async void CanRetrieveAndParseRefreshConfiguration()
+    {
+        unitTestClient.BaseAddress = new Uri("https://refresh.jvyden.xyz/api/v1/");
+        
+        RemoteConfiguration? remoteConfiguration = await lighthouseConfig.GetRemoteConfiguration();
+
+        if (remoteConfiguration == null)
+        {
+            Assert.Fail("Failed to retrieve remote configuration");
+        }
+
+        Assert.Equal("1138956002037866648", remoteConfiguration.ApplicationId);
+        Assert.Equal("LittleBigRefresh", remoteConfiguration.PartyIdPrefix);
+        Assert.Equal(UsernameType.Username, remoteConfiguration.UsernameType);
     }
 }
