d82ec230dafe2cf0860e199bec6f47d0b1c3fc68
koko <koko@drones.gay>
2023-08-15 09:23:47 -0400

    Attempt to correctly set macOS app bundle pwd

--- PLRPC.GUI.MacOS/Program.cs
+++ PLRPC.GUI.MacOS/Program.cs
@@ -5,6 +5,14 @@ public static class Program
     [STAThread]
     public static void Main()
     {
+        /*
+         * Attempt to set the current directory to the directory of the app bundle as
+         * macOS application bundles have their default pwd set to /.
+         *
+         * We'll just fall back to "../" if this fails.
+         */
+        Directory.SetCurrentDirectory(Path.GetDirectoryName(Environment.ProcessPath) ?? "../");
+        
         Gui.Initialize();
     }
 }
